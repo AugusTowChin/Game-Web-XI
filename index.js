2bb7dae903902bebe8d04403ef43982238a80931
AugusTowChin <iamaugust17172828@gmail.com>
2025-09-27 16:34:44 +0700

    <MESSAGE>

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function greetings() {
-    name = document.getElementById("username").value
-    console.log("Hello ", name)
+function PlayGame() {
+    userInput = document.getElementById("user-Input").value
+    console.log(userInput)
+
+    computerInput = Math.floor((Math.random() * 100) % 3)
+    console.log(computerInput)
+
+    if (userInput == computerInput) {
+        console.log("Draw")
+    } else if (userInput == 0 && computerInput == 2) {
+        // User Rock
+        // Com Scisssor
+        console.log("User Wins")
+    } else if (userInput == 1 && computerInput == 0) {
+        // User Paper
+        // Com Rock
+        console.log("User Wins")
+    } else if (userInput == 2 && computerInput == 1) {
+        // User Scissor
+        // Com Paper
+        console.log("User Wins")
+    } else {
+        console.log("Computer Wins")
+    }
+
+}
